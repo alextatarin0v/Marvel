c08b6e388ae6b0b8cb74e815073834db75a9cc87
Alex <starboyalxs@mail.ru>
2023-11-21 17:20:26 +0300

    175: creating dynamic paths, error output, v6+

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -26,7 +26,7 @@ const useMarvelService = () => {
         return res.data.results.map(_transformComics);
     };
 
-    const getComics = async (id) => {
+    const getComic = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
     };
@@ -55,7 +55,6 @@ const useMarvelService = () => {
                 : "No information about the number of pages",
             thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
             language: comics.textObjects[0]?.language || "en-us",
-            // optional chaining operator
             price: comics.prices[0].price
                 ? `${comics.prices[0].price}$`
                 : "not available",
@@ -69,7 +68,7 @@ const useMarvelService = () => {
         getAllCharacters,
         getCharacter,
         getAllComics,
-        getComics,
+        getComic,
     };
 };
 
