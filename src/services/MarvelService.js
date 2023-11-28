992df572c051db63f37e25de211b41ada5b42f3a
Alex <starboyalxs@mail.ru>
2023-11-28 18:08:45 +0300

    187: finite state machine principle

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
-    const { loading, request, error, clearError } = useHttp();
+    const { request, clearError, process, setProcess } = useHttp();
 
     const _apiBase = "https://gateway.marvel.com:443/v1/public/";
     const _apiKey = "apikey=414b45ae7abf6cf2e118625cb69c51eb";
@@ -50,9 +50,7 @@ const useMarvelService = () => {
         return {
             id: char.id,
             name: char.name,
-            description: char.description
-                ? `${char.description.slice(0, 210)}...`
-                : "There is no description for this character",
+            description: char.description ? `${char.description.slice(0, 210)}...` : "There is no description for this character",
             thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
@@ -65,21 +63,17 @@ const useMarvelService = () => {
             id: comics.id,
             title: comics.title,
             description: comics.description || "There is no description",
-            pageCount: comics.pageCount
-                ? `${comics.pageCount} p.`
-                : "No information about the number of pages",
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : "No information about the number of pages",
             thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
             language: comics.textObjects[0]?.language || "en-us",
-            price: comics.prices[0].price
-                ? `${comics.prices[0].price}$`
-                : "not available",
+            price: comics.prices[0].price ? `${comics.prices[0].price}$` : "not available",
         };
     };
 
     return {
-        loading,
-        error,
         clearError,
+        process,
+        setProcess,
         getAllCharacters,
         getCharacterByName,
         getCharacter,
